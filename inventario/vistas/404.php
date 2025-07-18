7ced413462fdc04c17a8317cc74c9d7809618f3a
fescubilla <escubillaflorencia8@gmail.com>
2025-07-18 11:03:27 -0300

    creacion de las vistas 404 y login

--- inventario/vistas/404.php
+++ inventario/vistas/404.php
@@ -1,1 +1,13 @@
-404
+<!-- aca vamos a crear el   -->
+<div class="main-container">
+    <section class="hero-body">
+	  	<div class="hero-body">
+		    <p class="title">
+		      Error 
+		    </p>
+		    <p class="subtitle">
+		      Pagina no encontrada
+		    </p>
+	  	</div>
+	</section>
+</div>
